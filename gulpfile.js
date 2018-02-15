a14762ec41246da727cd646b83a8a70227c59379
Serezon <letsplaygumbyscout@gmail.com>
2018-02-15 15:47:41 +0200

    Initial commit...

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'), 
+		sass = require('gulp-sass'),
+		browserSync = require('browser-sync');
+
+    gulp.task('sass', function(){ // Создаем таск Sass
+        return gulp.src('app/sass/**/*.sass') // Берем источник
+            .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+            .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+            .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+    });
+
+
+
+		gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    		browserSync({ // Выполняем browser Sync
+        		server: { // Определяем параметры сервера
+            		baseDir: 'app' // Директория для сервера - app
+        		},
+        		notify: false // Отключаем уведомления
+    		});
+		});
+
+    gulp.task('watch', ['browser-sync', 'sass'], function() {
+        gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+        gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+        gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+    });
+
