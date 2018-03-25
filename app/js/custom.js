930dc88899cfcdc97222c683591642b8db33b3b5
Serezon <pav.sered@gmail.com>
2018-03-25 22:06:32 +0300

    Using slick slider instead of unslider because... JUST BECAUSE(cause unslider did some shit that i don't like)

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,16 +1,10 @@
-// Gallery initialization
-jQuery(document).ready(function($) {
-			$('.gallery').unslider();
-		});
-   
-// Gallery config
-$('.gallery').unslider({
-	nav: false,
-	arrows: {
-		prev: '<a class="unslider-arrow prev"></a>',
-		next: '<a class="unslider-arrow next"></a>'
-	}
-});
+//Slick slider
+
+  $('.gallery').slick({
+  	prevArrow: '<button type="button" class="slick-prev"></button>',
+  	nextArrow: '<button type="button" class="slick-next"></button>'
+  });
+
 
 //Video
 
