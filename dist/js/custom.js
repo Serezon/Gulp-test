5b9b651db9769cd70567dc514579010409c0fcc2
Serezon <pav.sered@gmail.com>
2018-04-17 23:55:48 +0300

    Media-queries for max-width: 1024px

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,16 +1,10 @@
-// Gallery initialization
-jQuery(document).ready(function($) {
-			$('.gallery').unslider();
-		});
-   
-// Gallery config
-$('.gallery').unslider({
-	nav: false,
-	arrows: {
-		prev: '<a class="unslider-arrow prev"></a>',
-		next: '<a class="unslider-arrow next"></a>'
-	}
-});
+//Slick slider
+
+  $('.gallery').slick({
+  	prevArrow: '<button type="button" class="slick-prev"></button>',
+  	nextArrow: '<button type="button" class="slick-next"></button>'
+  });
+
 
 //Video
 
@@ -21,3 +15,32 @@ $('.video__preview').click(function() {
 $('.button').click(function() {
 	$('.video__preview').css('display','block');
 });
+
+
+// Плавный скролл по якорям
+$("a[href*='#']").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+
+// Hamburger
+
+$('#header__menu-burger').click(function() {
+  if ( $('.header__menu-list').css('display') == 'block' ) {
+    $('.header__menu-list').css('display','none');
+    $('#header__menu-burger').css('background-image', 'url("../img/icons/icon-menu.svg")');
+  } else {
+    $('.header__menu-list').css('display','block');
+    $('#header__menu-burger').css('background-image', 'url("../img/icons/icon-cross.svg")');
+  }
+});
+
+$('.header__menu-link').click(function() {
+  $('.header__menu-list').css('display', 'none');
+  $('#header__menu-burger').css('background-image', 'url("../img/icons/icon-menu.svg")');
+});
+
